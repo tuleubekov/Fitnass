01b3225bf23b0884e3790f5937f41a45af0118c9
Kudaybergen <tuleubekov.kas@gmail.com>
2018-12-27 06:11:08 +0600

    first commit

---
+++ app/src/main/java/com/akay/fitnass/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akay.fitnass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
