e08251755c8a84164ac1843626ad24941ebbe7e3
Kudaybergen <tuleubekov.kas@gmail.com>
2019-04-10 16:52:46 +0600

    Change background for item day, from selected to pressed state. Add background for new day button.

--- app/src/main/java/com/akay/fitnass/ui/main/WorkoutDayAdapter.java
+++ app/src/main/java/com/akay/fitnass/ui/main/WorkoutDayAdapter.java
@@ -1,7 +1,6 @@
 package com.akay.fitnass.ui.main;
 
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,13 +22,6 @@ public class WorkoutDayAdapter extends RecyclerView.Adapter<WorkoutDayAdapter.Vi
     private static final String FORMAT_DATETIME = "dd MMMM yyyy HH:mm";
     private List<Workout> mWorkouts;
     private OnItemClickListener mItemClickListener;
-    private int mSelected = -1;
-
-    /*
-     * Добавлено: при нажатии на итем изменяет его бэкграунд
-     * Есть проблема - неприятная задержка в отклике нажатия
-     * Проблема из-за фонового изображения
-     * */
 
     WorkoutDayAdapter(List<Workout> workoutList, OnItemClickListener listener) {
         mWorkouts = workoutList;
@@ -54,16 +46,7 @@ public class WorkoutDayAdapter extends RecyclerView.Adapter<WorkoutDayAdapter.Vi
         DateTimeFormatter format = DateTimeFormat.forPattern(FORMAT_DATETIME);
         viewHolder.textDate.setText(format.print(workout.getDate()));
         viewHolder.textRunCount.setText(String.valueOf(workout.getCount()));
-        viewHolder.layoutDay.setOnClickListener(view -> {
-            mItemClickListener.onItemClicked(workout.getId());
-            mSelected = viewHolder.getAdapterPosition();
-//            notifyDataSetChanged();
-        });
-        if (mSelected == pos) {
-            viewHolder.layoutDay.setSelected(true);
-        } else {
-            viewHolder.layoutDay.setSelected(false);
-        }
+        viewHolder.itemView.setOnClickListener(view -> mItemClickListener.onItemClicked(workout.getId()));
     }
 
     @Override
@@ -72,7 +55,6 @@ public class WorkoutDayAdapter extends RecyclerView.Adapter<WorkoutDayAdapter.Vi
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.layout_item_day) ConstraintLayout layoutDay;
         @BindView(R.id.text_date) TextView textDate;
         @BindView(R.id.text_run_count) TextView textRunCount;
 
