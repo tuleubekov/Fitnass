8ba64032e02419323d6c3420c1b19b72bfcc83a0
Kudaybergen <tuleubekov.kas@gmail.com>
2019-04-15 14:33:21 +0600

    Add CheckedButton. It is timer control button with changing bg by clicked

---
+++ app/src/main/java/com/akay/fitnass/ui/custom/CheckedButton.java
@@ -0,0 +1,48 @@
+package com.akay.fitnass.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+
+public class CheckedButton extends android.support.v7.widget.AppCompatButton implements Checkable {
+    private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
+
+    private boolean isChecked;
+
+    public CheckedButton(Context context) {
+        super(context);
+    }
+
+    public CheckedButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckedButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public int[] onCreateDrawableState(final int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+        refreshDrawableState();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void toggle() {
+        isChecked = !isChecked;
+        refreshDrawableState();
+    }
+}
