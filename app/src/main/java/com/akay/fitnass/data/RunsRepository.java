dd326d2558ba1d775c5c1bc7524833fcff865d93
Kudaybergen <tuleubekov.kas@gmail.com>
2019-05-02 13:40:32 +0600

    Create repository classes

---
+++ app/src/main/java/com/akay/fitnass/data/RunsRepository.java
@@ -0,0 +1,4 @@
+package com.akay.fitnass.data;
+
+public interface RunsRepository {
+}
---
+++ app/src/main/java/com/akay/fitnass/data/RunsRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.akay.fitnass.data;
+
+public class RunsRepositoryImpl implements RunsRepository {
+}
